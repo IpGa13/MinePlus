72f84560b70559da54dfedd6bbe0a1de1888645b
IpGa13 <83000938+IpGa13@users.noreply.github.com>
2023-10-13 23:31:26 +0200

    Init Project

---
+++ src/main/java/net/fredeun/mineplus/MinePlus.java
@@ -0,0 +1,17 @@
+package net.fredeun.mineplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinePlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
